5ed77eded869fa5adc092d8223948945beea4af5
Alberto Monterroso <Albermonte@users.noreply.github.com>
2017-02-26 18:02:50 +0100

    Add files via upload

---
+++ Practica3/cadena/src/palindromo.c
@@ -0,0 +1,12 @@
+#include "../include/palindromo.h"
+
+int quitarEspacios(const char *textoCon, char *textoSin)
+{
+	//Medir longitud de array
+	//For i<longitud
+	//Comparar cada elemento de array con ' ' y ','
+	//Si elemento==' 'o','
+	//For j=i;j<longitud;j++
+	//array[i]=array[i++]
+	//Fuera del for, longitud--
+}
