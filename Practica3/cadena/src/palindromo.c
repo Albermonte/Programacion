5e1a20807053dcb8b8675a3de0556faec934b0f7
Alberto Monterroso <Albermonte@users.noreply.github.com>
2017-02-26 18:22:01 +0100

    Update palindromo.c

--- Practica3/cadena/src/palindromo.c
+++ Practica3/cadena/src/palindromo.c
@@ -2,6 +2,31 @@
 
 int quitarEspacios(const char *textoCon, char *textoSin)
 {
+	int longitud, i, j;
+
+	longitud = strlen(line);
+
+	for (i = 0; i < longitud; i++)
+	{
+		if (line[i] == ' ')
+		{
+			for (j = i; j < longitud; j++)
+			{
+				line[j] = line[j+1];
+			}
+			longitud--;
+			i--;
+		}
+		if (line[i] == ',')
+		{
+			for (j = i; j < longitud; j++)
+			{
+				line[j] = line[j + 1];
+			}
+			longitud--;
+			i--;
+		}
+	}
 	//Medir longitud de array
 	//For i<longitud
 	//Comparar cada elemento de array con ' ' y ','
