a35f0b2b5c7a918d485f4307a728bad92bd3119c
Alberto Monterroso <alberto.monterroso@hotmail.com>
2017-03-16 16:11:44 +0100

    new file:   Practica6/Makefile 	new file:   Practica6/include/argumentos.h 	new file:   Practica6/include/operaciones.h 	new file:   Practica6/src/argumentos.c 	new file:   Practica6/src/argumentos.c~ 	new file:   Practica6/src/main.c 	new file:   Practica6/src/operaciones.c 	new file:   Practica6/src/operaciones.c~

---
+++ Practica6/include/operaciones.h
@@ -0,0 +1,17 @@
+/* Nombre Fichero: include/operaciones.h */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+// Declaracion de los prototipos de las funciones
+
+/* funcion que realiza la operación indicada en un char sobre los operandos pasados como argumentos
+ * [ IN] op1, Double primer operando de la operación 
+ * [ IN] op2, Double segundo operando de la operación 
+ * [ IN] operación, char que contiene la operación a realizar
+ * [RET] Menor que cero si hay error
+ */
+int realizarOperacion(double /*op1*/, double /*op2*/, char /*operacion*/);
---
+++ Practica6/src/argumentos.c
@@ -0,0 +1,37 @@
+/* Nombre Fichero: src/argumentos.c */
+
+#include "argumentos.h"
+
+int leerArgs(int argc, char **argv, double *op1, double *op2, char *operacion)
+{
+	if(strlen(argv[2])!=1)
+	{
+		return -1;
+	}
+	else
+	{
+	int i;
+	switch(argc)
+	{
+		case 4:
+			*op1=atof(argv[1]);
+			*op2=atof(argv[3]);
+			printf("Op:%s\n",argv[2]);
+			*operacion=argv[2][0];
+			break;
+		case 3:
+			for(i=0;i<10 /*strlen(argv[1][1])*/;i++)
+			{
+				if(argv[1][i]=='+')
+				{
+					strcpy(operacion,"+");
+					printf("Suma %s",operacion);
+				}
+				
+			break;
+			}		
+		
+	}
+	return 0;
+	}
+}
