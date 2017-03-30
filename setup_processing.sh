8a3e96ed085c772951acec516135e725cf8bc0a7
Alberto Monterroso <Albermonte@users.noreply.github.com>
2017-03-30 18:31:07 +0200

    Add files via upload

---
+++ setup_processing.sh
@@ -0,0 +1,12 @@
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+sudo update-alternatives --config java
+echo 
+echo 
+echo 
+echo 
+echo Listo!
+echo 
+echo 
+echo 
