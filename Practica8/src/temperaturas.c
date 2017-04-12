aa691a5dafd0455cafa1b546073c41a70eed0bf2
Alberto Monterroso <alberto.monterroso@hotmail.com>
2017-04-12 15:38:27 +0200

    modified:   Practica8/src/temperaturas.c

--- Practica8/src/temperaturas.c
+++ Practica8/src/temperaturas.c
@@ -1,183 +1,237 @@
-/* Nombre Fichero: src/temperaturas.c */
+/* Nombre Fichero: include/temperaturas.c */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #include "temperaturas.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define RESERVA_MEM 400
 
 
-#define RESERVA_MALLOC 400
-#define LONG_NOMBRE 20
-
-int leerFichero(const char* nombreRead, dateTemp_t** datos, int* numDatos, int* numLineasIncorrectas)
+int leerFichero(const char *nom, dateTemp_t **datos, int *num,  int *lin)
 {
-	if(nombreRead==NULL || datos==NULL || *numDatos==NULL || *numLineasIncorrectas==NULL)
+	FILE *pf=NULL;
+	int i=0,j=0;
+	int anyo=0,mes=0,dia=0;
+	float temperatura=0;
+	int numfcorrectas=0,numfincorrectas=0;
+	dateTemp_t *aux;
+	
+	
+	if ((pf=fopen(nom,"rb+"))==NULL)   //Abrir fichero 
+ 	{ 	//El fichero no existe, lo creamos
+   		if ((pf=fopen(nom, "wb+"))==NULL) { printf("Error de apertura de %s",nom); exit(1); }
+ 	}
+	if(ferror(pf)==1)
 	{
+		clearerr(pf);
+		fclose(pf);
 		return -1;
 	}
-	else
-	{
-		int i=1;
-		char ch;
-		FILE * fichero;
-		*numDatos=0;
-		*numLineasIncorrectas=0;
+
+	aux=(dateTemp_t*)malloc(RESERVA_MEM*sizeof(dateTemp_t));
+	*datos=(dateTemp_t*)malloc(RESERVA_MEM*sizeof(dateTemp_t));
+
+	rewind(pf);
 	
-		if((fichero=fopen(nombreRead,"r"))==NULL)
+	printf("aqui\n");
+					
+	do
+	{
+	fscanf(pf,"%d-%d-%d\t%f\n",&anyo,&mes,&dia,&temperatura);
+	
+		*datos=realloc(*datos,RESERVA_MEM*j*sizeof(dateTemp_t));
+		aux=realloc(*datos,RESERVA_MEM*j*sizeof(dateTemp_t));
+	
+		if(anyo>=1988 && anyo<=1991 && mes>=0 && mes<=12 && dia>=0 && dia<=31 && temperatura>-100 && temperatura<100)
+		{	
+			aux[i].momento.anyo=anyo;
+			aux[i].momento.mes=mes;
+			aux[i].momento.dia=dia;
+			aux[i].temperatura=temperatura;
+			
+			numfcorrectas++;
+		}
+		
+		if(anyo<1988||anyo>1991||mes<0||mes>12||dia<0||dia>31||temperatura<-100||temperatura>100)
+		{
+			printf("dato no guardado %d %d %d\t%f\n",anyo,mes,dia,temperatura);
+			numfincorrectas++;
+		}
+		i++;
+		j++;
+		
+		
+	}while (!feof(pf) && !ferror(pf));
+
+	for(i=0;i<numfcorrectas;i++)
+	{
+		(*datos)[i].momento.anyo=aux[i].momento.anyo;
+		(*datos)[i].momento.mes=aux[i].momento.mes;
+		(*datos)[i].momento.dia=aux[i].momento.dia;
+		(*datos)[i].temperatura=aux[i].temperatura;
+	}
+
+	*num=numfcorrectas;
+	*lin=numfincorrectas;
+	
+	return 0;
+}
+
+
+int guardarFichero(const dateTemp_t *dat,  int num, const char *nom)
+{
+	FILE *pf;
+
+	if(dat==NULL||num<=0||nom==NULL)
+		return -1;
+
+
+	if ((pf=fopen(nom,"rb+"))==NULL)   //Abrir fichero 
+ 	{ 	//El fichero no existe, lo creamos
+   		if ((pf=fopen(nom, "wb+"))==NULL) { printf("Error de apertura de %s",nom); exit(1); }
+ 	}
+	if(ferror(pf)==1)
+	{
+		clearerr(pf);
+		fclose(pf);
+		return -1;
+	}
+	rewind(pf);
+	fwrite(dat,sizeof(dateTemp_t),num,pf);		//Copiamos los elementos
+	fclose(pf);
+	return 0;
+}
+
+
+int modificarRegistro(dateTemp_t **dat, int num, struct fecha_t fec, float temp)
+{
+
+	int i=0,registro=0;
+
+	for(i=0;i<num;i++)
+	{
+		if((*dat)[i].momento.anyo==fec.anyo && (*dat)[i].momento.mes==fec.mes && (*dat)[i].momento.dia==fec.dia)
+			registro=i;
+	}
+
+	(*dat)[registro].temperatura=temp;
+
+	if((*dat)[registro].momento.anyo<1988||(*dat)[registro].momento.anyo>1991||(*dat)[registro].momento.mes<0||(*dat)[registro].momento.mes>12||(*dat)[registro].momento.dia<0||(*dat)[registro].momento.dia>31||(*dat)[registro].temperatura<-100||(*dat)[registro].temperatura>100)
 		{
 			return -1;
 		}
-	
-		*datos=(dateTemp_t *)calloc(sizeof(dateTemp_t),RESERVA_MALLOC);
-	
-		if(*datos==NULL)
-		{
-			free(*datos);
-			return -1;
-		}
-	
-		while(!feof(fichero))
-		{
-	  		ch = fgetc(fichero);
-	  		if(ch == '\n')
-	  		{
-	    			(*datos)[*numDatos].momento.anyo=0;
-				(*datos)[*numDatos].momento.mes=0;
-				(*datos)[*numDatos].momento.dia=0;
-				(*datos)[*numDatos].temperatura=0;
-			
-				fscanf(fichero,"%d-%d-%d\t%f",&(*datos)[*numDatos].momento.anyo,&(*datos)[*numDatos].momento.mes,&(*datos)[*numDatos].momento.dia,&(*datos)[*numDatos].temperatura);
-			
-				if((*datos)[*numDatos].momento.anyo<1988 || (*datos)[*numDatos].momento.anyo>1991|| (*datos)[i].momento.mes<1 || (*datos)[i].momento.mes>12 || (*datos)[*numDatos].momento.dia<1 || (*datos)[*numDatos].momento.dia>31 || (*datos)[*numDatos].temperatura<-100 || (*datos)[*numDatos].temperatura>100)
-				{
-					*numLineasIncorrectas=*numLineasIncorrectas+1;
-				}
-				else
-				{
-					*numDatos=*numDatos+1;
-				}
-			
-				printf("%d\n",*numDatos);
-			
-				if(*numDatos==RESERVA_MALLOC*i)			//realloc
-				{
-					i++;
-					*datos=(dateTemp_t *)realloc(*datos,RESERVA_MALLOC*i);
-					if(*datos==NULL)
-					{
-						free(*datos);
-						return -1;
-					}
-				}
-	 		}
-	    	}
-    }	
-}
 
-int calcularTempMaxima(const dateTemp_t* datos, int numDatos, float* temp, struct fecha_t* diaM)
-{
-if(datos==NULL || numDatos<1 || temp==NULL || diaM==NULL)
-{
-	return -1;
-}
-else
-{
-      int i;
-      *temp=0;
-      for(i=0;i<numDatos;i++)
-      {
-      	if(*temp<datos[i].temperatura)
-      	{
-      		*temp=datos[i].temperatura;
-      		(*diaM).anyo=datos[i].momento.anyo;
-      		(*diaM).mes=datos[i].momento.mes;
-      		(*diaM).dia=datos[i].momento.dia;
-      	}
-      }
-      return 0;
-}
-}
-
-int calcularTempMinima(const dateTemp_t* datos, int numDatos, float* temp, struct fecha_t* diaM)
-{
-if(datos==NULL || numDatos<1 || temp==NULL || diaM==NULL)
-{
-	return -1;
-}
-else
-{
-      int i;
-      *temp=datos[0].temperatura;
-      for(i=0;i<numDatos;i++)
-      {
-      	if(*temp>datos[i].temperatura)
-      	{
-      		*temp=datos[i].temperatura;
-      		(*diaM).anyo=datos[i].momento.anyo;
-      		(*diaM).mes=datos[i].momento.mes;
-      		(*diaM).dia=datos[i].momento.dia;
-      	}
-      }
-      return 0;
-}
-}
-
-int calcularTempMedia(const dateTemp_t* datos, int numDatos, float* temp)
-{
-if(datos==NULL || numDatos<1 || temp==NULL)
-{
-	return -1;
-}
-else
-{
-	int i;
-	*temp=0;
-	for(i=0;i<numDatos;i++)
-	{
-	*temp=*temp+datos[i].temperatura;
-	}
-	*temp=*temp/numDatos;
 	return 0;
 }
-}
 
-int guardarFichero(const dateTemp_t* datos, int numDatos, const char* nombre)
-{
-if(datos==NULL || numDatos<0 || nombre==NULL)
-{
-	return -1;
-}
-else
-{
-	FILE * fp;
-	int i;
-	fp=fopen(nombre,"w");
-	for(i=0;i<numDatos;i++)
-	{
-	fprintf(fp,"%d-%d-%d\t%f",datos[i].momento.anyo,datos[i].momento.mes,datos[i].momento.dia,datos[i].temperatura);
-	}
-	fclose(fp);
-	return 0;
-}
-}
 
-int modificarRegistro(dateTemp_t** datos, int numDatos, struct fecha_t diaM, float temp)
+int calcularTempMaxima(const dateTemp_t *dat, int num, float *temp, struct fecha_t *fec)
 {
-if(datos==NULL || numDatos<0)
-{
-	return -1;
-}
-else
-{
-	int i;
-	for(i=0;i<numDatos;i++)
+	int i=0,registro=0;
+	float max;
+
+	if(dat==NULL)
+		return-1;
+
+	if(num<=0)
+		return -1;
+
+	for(i=0;i<num;i++)
 	{
-		if((*datos)[i].momento.dia==diaM.dia)
+		if(dat[i].temperatura>max && dat[i].temperatura>-100 && dat[i].temperatura<100 && dat[i].momento.anyo>=1988 && dat[i].momento.anyo<=1991 && dat[i].momento.mes>=0 && dat[i].momento.mes<=12 && dat[i].momento.dia>=0 && dat[i].momento.dia<=31)
 		{
-			(*datos)[i].temperatura=temp;
+			max=dat[i].temperatura;
+			registro=i;
 		}
 	}
+
+	*fec=dat[registro].momento;
+	*temp=max;
 	return 0;
 }
+
+
+
+
+int calcularTempMinima(const dateTemp_t *dat, int num, float *temp, struct fecha_t *fec)
+{
+	int i=0,registro=0;
+	float min=0;
+
+	if(dat==NULL)
+		return-1;
+
+	if(num<=0)
+		return -1;
+
+	for(i=0;i<num;i++)
+	{
+		if(dat[i].temperatura<min && dat[i].temperatura>-100 && dat[i].temperatura<100 && dat[i].momento.anyo>=1988 && dat[i].momento.anyo<=1991 && dat[i].momento.mes>=0 && dat[i].momento.mes<=12 && dat[i].momento.dia>=0 && dat[i].momento.dia<=31)
+		{
+			min=dat[i].temperatura;
+			registro=i;
+		}
+	}
+	*fec=dat[registro].momento;
+	*temp=min;
+	return 0;
 }
+
+
+int calcularTempMedia(const dateTemp_t *dat, int num, float *temp)
+{
+	double suma=0;
+	int i=0,j=0;
+	
+	if(dat==NULL)
+		return-1;
+
+	
+	if(num<=0)
+		return -1;
+
+	for(i=0;i<num;i++)
+	{
+		if(dat[i].temperatura>-100 && dat[i].temperatura<100 && dat[i].momento.anyo>=1988 && dat[i].momento.anyo<=1991 && dat[i].momento.mes>=0 && dat[i].momento.mes<=12 && dat[i].momento.dia>=0 && dat[i].momento.dia<=31)
+		{
+			suma=dat[i].temperatura+suma;
+			j++;
+		}
+	}
+	*temp=suma/num;
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
